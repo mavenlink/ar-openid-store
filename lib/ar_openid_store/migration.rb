0a3b2314c490fa1a770033ae0c6a880ce1e300a6
Rémi Prévost <remi@exomel.com>
2012-11-19 15:27:15 -0500

    Clean trailing whitespace

--- lib/ar_openid_store/migration.rb
+++ lib/ar_openid_store/migration.rb
@@ -1,7 +1,7 @@
 module ArOpenidStore
-  
+
   module Migration
-    
+
     def create_openid_tables
       create_table "open_id_associations", :force => true do |t|
         t.column "server_url", :binary
@@ -18,14 +18,14 @@ module ArOpenidStore
         t.column :salt, :string, :null => false
       end
     end
-    
+
     def drop_openid_tables
       drop_table "open_id_associations"
       drop_table "open_id_nonces"
     end
-    
+
   end
-  
+
 end
 
 ActiveRecord::Migration.extend ArOpenidStore::Migration
