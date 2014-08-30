4189ce937391aa216c4b53441230f160d4e9eff0
Alan Heywood <alan@softweb.net.au>
2014-08-30 16:14:29 +1000

    Remove attr_accessible for rails 4

--- lib/ar_openid_store/association.rb
+++ lib/ar_openid_store/association.rb
@@ -4,7 +4,6 @@ require 'time'
 module ArOpenidStore
   class Association < ActiveRecord::Base
     self.table_name = 'open_id_associations'
-    attr_accessible :server_url, :handle, :secret, :issued, :lifetime, :assoc_type
 
     def from_record
       OpenID::Association.new(handle, secret, Time.at(issued), lifetime, assoc_type)
--- lib/ar_openid_store/nonce.rb
+++ lib/ar_openid_store/nonce.rb
@@ -1,6 +1,5 @@
 module ArOpenidStore
   class Nonce < ActiveRecord::Base
     self.table_name  = 'open_id_nonces'
-    attr_accessible :server_url, :timestamp, :salt
   end
 end
